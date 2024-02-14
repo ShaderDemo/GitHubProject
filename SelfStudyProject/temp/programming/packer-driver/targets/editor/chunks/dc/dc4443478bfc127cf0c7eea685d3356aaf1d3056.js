4c555fdd1343380a023079809d09b36ff9bccc8d
张小强 <zhangxiaoqiang@zhangxiangdeMBP.lan>
2024-02-14 11:31:58 +0800

    Learning Project Creation - First Upload
    学习项目创建-首次上传

---
+++ SelfStudyProject/temp/programming/packer-driver/targets/editor/chunks/dc/dc4443478bfc127cf0c7eea685d3356aaf1d3056.js
@@ -0,0 +1,39 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, _dec, _class, _crd, ccclass, property, NewComponent;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "e8b962UVd1NRY/9Q1rB12qb", "NewComponent", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("NewComponent", NewComponent = (_dec = ccclass('NewComponent'), _dec(_class = class NewComponent extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=dc4443478bfc127cf0c7eea685d3356aaf1d3056.js.map
